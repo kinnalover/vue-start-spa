9abf3843e137e52f7b142237c3969a5572f50c8e
Zhandos <ZhandosAb2@halykbank.kz>
2024-03-06 16:22:26 +0500

    last

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => activePage = index"
+  ></navbar>
+  <page-viewer 
+    :page="pages[activePage]"
+  ></page-viewer>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageViewer from "./components/PageViewer.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Navbar,
+    PageViewer,
+    
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data() { 
+    return {
+      activePage: 0,
+      pages: [
+        {
+          link: { text: "Home", url: "index.html" },
+          pageTitle: "Home Page",
+          content: "this is the home ",
+        },
+        {
+          link: { text: "About", url: "about.html" },
+          pageTitle: "About Page",
+          content: "this is the about content ",
+        },
+        {
+          link: { text: "Contact", url: "content.html" },
+          pageTitle: "Content Page",
+          content: "this is the contact ",
+        }
+      ]
+    };
+  },
+};
+</script>
