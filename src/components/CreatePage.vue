ae23ded4331afdc3ca749a126c08b951ef0b2bcf
Zhandos <ZhandosAb2@halykbank.kz>
2024-03-07 14:13:48 +0500

    L4.6-start

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -1,20 +1,44 @@
 <template>
   <div class="container mb-3">
     <form action="">
-      <div class="mb-3">
-        <label for="" class="form-label"> Page Title </label>
-        <input type="text" class="form-control" v-model="pageTitle"/>
-      </div>
-      <div class="mb-3">
-        <label for="" class="form-label"> Content </label>
-        <textarea type="text" class="form-control" rows="5" v-model="content"></textarea>
+      <div class="row">
+        <div class="col-md-8">
+          <div class="mb-3">
+            <label for="" class="form-label"> Page Title </label>
+            <input type="text" class="form-control" v-model="pageTitle" />
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label"> Content </label>
+            <textarea
+              type="text"
+              class="form-control"
+              rows="5"
+              v-model="content"
+            ></textarea>
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-3">
+            <label for="" class="form-label"> Link Text </label>
+            <input type="text" class="form-control" v-model="linkText" />
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">Link URL</label>
+            <input type="text" class="form-control" v-model="linkUrl" />
+          </div>
+          <div class="row mb-3">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" />
+              <label class="from-check-label" for="gridCheck1">
+                Published
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="mb-3">
-        <button
-          class="btn btn-primary"
-          @click.prevent="pageCreated({ pageTitle, content})"
-        >
+        <button class="btn btn-primary" :disabled="isFormInvalid" @click.prevent="submitForm">
           Create Page
         </button>
       </div>
@@ -25,11 +49,34 @@
 <script>
 export default {
   props: ["pageCreated"],
+  computed: {
+      isFormInvalid(){
+          return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl;
+      }
+  },
   data() {
     return {
       pageTitle: "",
-      content: ""
+      content: "",
+      linkText: "",
+      linkUrl: "",
     };
+  },
+  methods: {
+    submitForm() {
+      if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+        alert("blya vse zapolni! ");
+        return;
+      }
+      this.pageCreated({
+        pageTitle: this.pageTitle,
+        content: this.content,
+        link: {
+          text: this.linkText,
+          url: this.linkUrl,
+        },
+      });
+    },
   },
 };
 </script>
