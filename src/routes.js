4b73e49497d7a06804e25b2d7cde55647f8c060a
kinnalover <kinnalover@gmail.com>
2024-03-17 01:36:11 +0500

    route that receives props

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import PageViewer from "./views/PageViewer.vue";
 import CreatePage from "./views/CreatePage.vue";
 import Pages from "./views/Pages.vue";
 import PagesList from "./views/PagesList.vue";
+import PageEdit from "./views/PageEdit.vue";
+
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
@@ -13,6 +15,7 @@ const router = createRouter({
             children: [
                 {path: '',component: PagesList},
                 {path: 'create',component: CreatePage},
+                {path: ':index/edit', component: PageEdit, props: true}
             ]
         },
 
--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,12 +1,9 @@
 <template>
     <h4>Pages</h4>
     <div class="text-end">
-        <router-link
-            to="/pages/create"
-            class="btn btn-primary btn-sm"
-        >New Page</router-link>
+        <router-link to="/pages/create" class="btn btn-primary btn-sm">New Page</router-link>
     </div>
-    <table class="table table-striped table-hover">
+    <table class="table table-hover">
         <thead>
             <tr>
                 <th>Title</th>
@@ -15,38 +12,31 @@
             </tr>
         </thead>
         <tbody>
-            <tr
-                v-for="(page, index) in $pages.getAllPages()"
-            :key="index"
-            >
-                <td>{{page.pageTitle }}</td>
-                <td>{{ page.link.text}}</td>
-                <td>{{ page.published ? 'yes' :'no' }}</td>
+            <tr v-for="(page, index) in $pages.getAllPages()" :key="index" @click="goToPage(index)">
+                <td>{{ page.pageTitle }}</td>
+                <td>{{ page.link.text }}</td>
+                <td>{{ page.published ? 'yes' : 'no' }}</td>
             </tr>
         </tbody>
     </table>
 </template>
 <script setup>
-import {ref, reactive, inject} from 'vue';
-
-// let counter = ref(0);
+import { ref, reactive, inject } from 'vue';
+import { useRouter } from 'vue-router';
 
 const data = reactive({
     counter: 0
 })
 const $pages = inject('$pages');
+const router = useRouter();
 
+function goToPage(index) {
+    router.push({ path: `pages/${index}/edit` });
+}
 
-// export default {
-//     data(){
-//         return {
-//             counter: 0
-//         }
-//     },
-//     methods: {
-//         click(){
-//             this.counter++;
-//         }
-//     }
-// }
 </script>
+<style scoped>
+.table.table-hover tr:hover {
+    cursor: pointer;
+}
+</style>
